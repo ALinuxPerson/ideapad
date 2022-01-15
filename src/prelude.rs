d10ef4c5e5739f35b617d131b8526360164236c2
ALinuxPerson <alinuxperson@gmail.com>
2022-01-15 19:30:07 +0800

    Add more items to `prelude`

--- src/prelude.rs
+++ src/prelude.rs
@@ -3,10 +3,23 @@
 pub use crate::{
     acpi_call::{Error as AcpiCallError, Result as AcpiCallResult},
     battery_conservation::{
-        BatteryConservationController, Error as BatteryConservationModeError,
+        BatteryConservationController,
+        Error as BatteryConservationModeError,
         Result as BatteryConservationModeResult,
     },
-    profile::{Error as ProfileError, Profile, Result as ProfileResult},
+    context::Context,
+    fallible_drop_strategy::{
+        DynFallibleDropStrategy,
+        FallibleDropStrategy,
+        ThreadSafeWrite,
+        ThreadSafe,
+        FallibleDropStrategies,
+    },
+    profile::{
+        Error as ProfileError,
+        Profile,
+        Result as ProfileResult
+    },
     rapid_charge::{Error as RapidChargeError, RapidChargeController, Result as RapidChargeResult},
     system_performance::{
         Error as SystemPerformanceModeError, Result as SystemPerformanceModeResult,
