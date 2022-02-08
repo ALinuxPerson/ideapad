0e4899eb885e41c121bf7b792cfa1b077741dbe7
ALinuxPerson <alinuxperson@gmail.com>
2022-02-08 15:32:49 +0800

    Use try drop for battery conservation

--- src/battery/private.rs
+++ src/battery/private.rs
@@ -1,10 +1,16 @@
 use crate::battery_conservation::BatteryConservationEnableGuard;
 use crate::rapid_charge::RapidChargeEnableGuard;
 use crate::{BatteryConservationController, RapidChargeController};
+use try_drop::prelude::*;
 
 pub trait BatteryEnableGuardSeal {}
 
-impl<'bc, 'ctx: 'bc> BatteryEnableGuardSeal for BatteryConservationEnableGuard<'bc, 'ctx> {}
+impl<'bc, 'ctx, D, DD> BatteryEnableGuardSeal for BatteryConservationEnableGuard<'bc, 'ctx, D, DD>
+where
+    'ctx: 'bc,
+    D: FallibleTryDropStrategy,
+    DD: FallbackTryDropStrategy,
+{}
 
 impl<'rc, 'ctx: 'rc> BatteryEnableGuardSeal for RapidChargeEnableGuard<'rc, 'ctx> {}
 
@@ -12,6 +18,10 @@ pub trait BatteryDisableGuardSeal {}
 
 pub trait BatteryControllerSeal {}
 
-impl<'ctx> BatteryControllerSeal for BatteryConservationController<'ctx> {}
+impl<'ctx, D, DD> BatteryControllerSeal for BatteryConservationController<'ctx, D, DD>
+where
+    D: FallibleTryDropStrategy,
+    DD: FallbackTryDropStrategy,
+{}
 
 impl<'ctx> BatteryControllerSeal for RapidChargeController<'ctx> {}
